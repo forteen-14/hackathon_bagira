15cfcb471e29c6f4c6bd41f16968a54dd83755fc
forteen14 <nh-shalevm@ortarad.ort.org.il>
2023-08-30 11:31:46 +0300

    update stuff

--- client.py
+++ client.py
@@ -5,13 +5,24 @@ import consts
 def start_connection():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((consts.IP, consts.PORT))
+    client.sendall(bytes("This is from Client", 'UTF-8'))
     return client
 
 
 def keep_connection(client):
     while True:
         in_data = client.recv(1024)
-        print("From Server :", in_data.decode())
+        sever_msg = in_data.decode()
+        server_code = sever_msg[0:2]
+        if server_code == consts.SEND:
+            pass
+        elif server_code == consts.LOGIN:
+            #CODE#username CODE#CATEGORY:{USERNAME:TEXT:USERNAME:TEXT}#CATEGORY:{USERNAME:TEXT:USERNAME:TEXT}
+
+
+        elif server_code == consts.UPLOAD_MSG_AT_LOGIN:
+            pass
+        print("From Server :", sever_msg)
         out_data = input()
         client.sendall(bytes(out_data, 'UTF-8'))
         if out_data == 'bye':
