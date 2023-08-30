dbda1dfbdd7cdbc35bf2a5314ed012e0b45c5896
forteen14 <nh-shalevm@ortarad.ort.org.il>
2023-08-30 10:11:06 +0300

    add db

---
+++ db.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+
+def create_table():
+    db = sqlite3.connect('db.db')
+    db.execute('''CREATE TABLE IF NOT EXISTS user (name TEXT, help_given INT, help_got INT);''')
+    db.commit()
+    return db
+
+
+def add_user(name, db):
+    db.execute('''INSERT INTO user (name, help_given, help_got) VALUES (?, ?, ?);''', (name, 0, 0))
+    db.commit()
+
+
+
+#check if user exists
+def user_exists(name, db):
+    cursor = db.execute('''SELECT * FROM user WHERE name=?;''', (name,))
+    return len(cursor.fetchall()) > 0
+
+
+def main():
+    db = create_table()
+    add_user('test', db)
+    db.close()
+
+
+if __name__ == '__main__':
+    main()
