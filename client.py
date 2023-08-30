f439d8548b78f9b15473c327b25db72b3b0778ae
forteen14 <nh-shalevm@ortarad.ort.org.il>
2023-08-30 10:59:06 +0300

    update stuff

--- client.py
+++ client.py
@@ -2,27 +2,29 @@ import socket
 import consts
 
 
-
 def start_connection():
-  client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  client.connect((consts.HOST, consts.PORT))
-  return client
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((consts.HOST, consts.PORT))
+    return client
+
 
 def keep_connection(client):
-  while True:
-    in_data = client.recv(1024)
-    print("From Server :", in_data.decode())
-    out_data = input()
-    client.sendall(bytes(out_data, 'UTF-8'))
-    if out_data == 'bye':
-      break
-  client.close()
+    while True:
+        in_data = client.recv(1024)
+        print("From Server :", in_data.decode())
+        out_data = input()
+        client.sendall(bytes(out_data, 'UTF-8'))
+        if out_data == 'bye':
+            break
+    client.close()
 
 
 def main():
-    client = start_connection()
-    keep_connection(client)
-
+    try:
+        client = start_connection()
+        keep_connection(client)
+    except:
+        print("ERROR: Connection failed")
 
 
 if __name__ == '__main__':
