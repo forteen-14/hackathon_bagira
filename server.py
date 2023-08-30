128a152a62a43eeacd9a54809f8a85e3066e35d2
h <dan086421@gmail.com>
2023-08-30 10:57:01 +0300

    ignore

--- server.py
+++ server.py
@@ -16,15 +16,22 @@ class ClientThread(threading.Thread):
             print ("from client", msg)
             self.csocket.send(bytes(msg,'UTF-8'))
         print ("Client at ", clientAddress , " disconnected...")
-LOCALHOST = "127.0.0.1"
-PORT = 8080
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind((LOCALHOST, PORT))
-print("Server started")
-print("Waiting for client request..")
-while True:
-    server.listen(1)
-    clientsock, clientAddress = server.accept()
-    newthread = ClientThread(clientAddress, clientsock)
-    newthread.start()
+def main():
+    LOCALHOST = "127.0.0.1"
+    PORT = 8080
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((LOCALHOST, PORT))
+    print("Server started")
+    print("Waiting for client request..")
+    while True:
+        server.listen(1)
+        clientsock, clientAddress = server.accept()
+        newthread = ClientThread(clientAddress, clientsock)
+        newthread.start()
+
+
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
