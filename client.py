74dec0f17f1f0d12b483446d45437aa5f682618a
h <dan086421@gmail.com>
2023-08-30 13:23:51 +0300

    server

--- client.py
+++ client.py
@@ -1,17 +1,43 @@
 import socket
 import consts
+import user
 
 
 def start_connection():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((consts.IP, consts.PORT))
+    client.sendall(bytes("This is from Client", 'UTF-8'))
     return client
+
+
+def handle_client_by_code(server_code, data):
+    if server_code == consts.SEND:
+        # CODE#username#category#subject#descreption
+        pass
+    elif server_code == consts.LOGIN:
+        # CODE#username
+        # TO-DO IN GUI
+        pass
+    elif server_code == consts.UPLOAD_MSG_AT_LOGIN:
+        # CODE#username CODE#CATEGORY:{USERNAME:TEXT:USERNAME:TEXT}#CATEGORY:{USERNAME:TEXT:USERNAME:TEXT}
+        list_of_categories = data.split("#")
+        for category in list_of_categories:
+            list_of_messages = category.split(",")
+            for message in list_of_messages:
+                # show the message in gui
+                pass
+    elif server_code == consts.UPLOAD_MSG_AT_LOGIN:
+        # CODE#username CODE#CATEGORY:{USERNAME:TEXT:USERNAME:TEXT}
+        pass
 
 
 def keep_connection(client):
     while True:
         in_data = client.recv(1024)
-        print("From Server :", in_data.decode())
+        sever_msg = in_data.decode()
+        server_code = sever_msg[0:2]
+        handle_client_by_code(server_code, sever_msg[2:])
+        print("From Server :", sever_msg)
         out_data = input()
         client.sendall(bytes(out_data, 'UTF-8'))
         if out_data == 'bye':
--- server.py
+++ server.py
@@ -3,14 +3,22 @@ import socket, threading
 import db
 
 dataBase=db.DataBase()
+#msg={
+# "category1':[[user1:msg1],[user2:msg2]]
+# "category2':[[user3:msg31],[user4:msg4]]
+# }
 
+#keep_last_msg={
+# "category1':[user1,msg1]
+# "category2':[user2:msg2]
+# }
 class ClientThread(threading.Thread):
     def __init__(self,clientAddress,clientsocket,msgs,keep_last_msg):
         threading.Thread.__init__(self)
         self.csocket = clientsocket
         self.clientAddress=clientAddress
         self.msgs=msgs
-        self.keep_last_msf=keep_last_msg
+        self.keep_last_msg=keep_last_msg
         self.username=""
         print ("New connection added: ", clientAddress)
 
@@ -28,17 +36,40 @@ class ClientThread(threading.Thread):
         print ("Client at ", self.clientAddress , " disconnected...")
 
 
-    def send_handle(self,data):
-        username,category,subject,description=data
-        self.msgs[category].appand(data)
+    def sent_handle(self,data):
+        username,category,description=data
+        msg={username:description}
+        self.msgs[category].appand(msg)
+        self.keep_last_msg[category]=[username,description]
 
 
+    def make_last_msgs_packet(self):
+        str=f"{consts.LAST_MSGS}#"
+        for category,chat in self.keep_last_msg.items():
+            name,msg=chat
+            str+=f"{category}:[{name}:{msg}]#"
+        print(str)
+        return str
 
     def login_handle(self,name):
         self.username=name
-        dataBase.add_user(name)
-    def uplode_at_login_handle(self,data):
-        pass
+        res=""
+        if not dataBase.user_exists(name):  # user not exist
+            dataBase.add_user(name)
+        name,help_got,help_given=db.getUser(name)
+        res+=f"{consts.LOGIN}#{name}#{help_got}#{help_given}#"
+        res+=self.uplode_at_login_handle()
+        print(res)
+        return res
+    def uplode_at_login_handle(self):
+        res=("")
+        for catgory,chat in self.msgs.items():
+            res += f"{catgory}:["
+            for msg in chat:
+                res+=f"{msg[0]}:{msg[1]}:"
+            res+="]"
+
+        return res
     def handle_client(self,data):
         msg = data.decode()
         res=""
@@ -47,13 +78,9 @@ class ClientThread(threading.Thread):
         print(f"from client: {msg}")
         msg_split=msg.split("#")
         if msg_split==consts.SEND:
-            res=self.send_handle(msg[1:])
+            res=self.sent_handle(msg[-1])
         elif msg_split==consts.LOGIN:
-            self.login_handle(msg[1:])
-        elif msg_split==consts.UPLOAD_MSG_AT_LOGIN:
-            res=self.uplode_at_login_handle(msg[1:])
-
-
+            res=self.login_handle(msg[1:])
 
         return True,res
 
